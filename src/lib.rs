b07b07724d9fa13ab037c4846ef415aff06aac66
Goblin <39985547+ms0uto@users.noreply.github.com>
2023-11-20 01:01:43 +0100

    feat: refactor health_check route and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health() -> impl Responder {
-    HttpResponse::Ok()
-}
+use actix_web::{web, App, HttpResponse, HttpServer};
 
 pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/", web::get().to(health)))
-        .bind("127.0.0.1:8000")?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new().route(
+            "/health_check",
+            web::get().to(|| async { HttpResponse::Ok() }),
+        )
+    })
+    .bind("127.0.0.1:8000")?
+    .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,17 +4,18 @@ use reqwest::Client;
 async fn health_works() {
     spawn_app();
 
-    let response = Client::new()
-        .get("http://127.0.0.1:8000/health")
+    let client = Client::new();
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
         .send()
         .await
         .expect("Failed to execute request.");
 
     assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
+    assert_eq!(Some(0), response.content_length())
 }
 
 fn spawn_app() {
     let server = newsletter_service::run().expect("Failed to bind address");
-    tokio::spawn(server);
+    let _ = tokio::spawn(server);
 }
