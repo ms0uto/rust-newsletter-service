6337e91bd818d669cdde5c956cba415aab2de79d
Goblin <39985547+ms0uto@users.noreply.github.com>
2023-11-20 01:06:39 +0100

    fix: clippy compliance, remove async for http response ok

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,9 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 
 pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new().route(
-            "/health_check",
-            web::get().to(|| async { HttpResponse::Ok() }),
-        )
-    })
-    .bind("127.0.0.1:8000")?
-    .run();
+    let server =
+        HttpServer::new(|| App::new().route("/health_check", web::get().to(|| HttpResponse::Ok())))
+            .bind("127.0.0.1:8000")?
+            .run();
     Ok(server)
 }
