bfa068dba6edb103679d337a2ddd760a2643d5ae
Goblin <39985547+ms0uto@users.noreply.github.com>
2023-11-20 23:02:03 +0100

    feat: postgres database init script and create migration

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
-pub fn run() -> Result<Server, std::io::Error> {
-    let server =
-        HttpServer::new(|| App::new().route("/health_check", web::get().to(|| HttpResponse::Ok())))
-            .bind("127.0.0.1:8000")?
-            .run();
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(|| HttpResponse::Ok()))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
