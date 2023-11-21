42983ebf32d258b20c1802032065834b1e56044b
Goblin <39985547+ms0uto@users.noreply.github.com>
2023-11-21 23:41:31 +0100

    feat: add configuration file and wiring for postgress and sqlx, updated general  workflow

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use newsletter_service::run;
+use newsletter_service::configuration::get_configuration;
+use newsletter_service::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run(TcpListener::bind("127.0.0.1:0").expect("Failed to bind port"))?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    run(TcpListener::bind(address).expect("Failed to bind port"))?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,7 @@
+use newsletter_service::configuration::get_configuration;
+use newsletter_service::startup;
 use reqwest::Client;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 #[tokio::test]
@@ -18,7 +21,7 @@ async fn health_check_works() {
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
     let port = listener.local_addr().unwrap().port();
-    let server = newsletter_service::run(listener).expect("Failed to bind address");
+    let server = startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
@@ -26,6 +29,11 @@ fn spawn_app() -> String {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -38,6 +46,13 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
@@ -50,7 +65,6 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         ("", "missing both name and email"),
     ];
     for (invalid_body, error_message) in test_cases {
-        // Act
         let response = client
             .post(&format!("{}/subscriptions", &app_address))
             .header("Content-Type", "application/x-www-form-urlencoded")
