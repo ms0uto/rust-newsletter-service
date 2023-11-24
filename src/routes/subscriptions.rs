9e210d1c5668b13d790a6a2604901fd64b6f3a4e
Goblin <39985547+ms0uto@users.noreply.github.com>
2023-11-24 19:36:18 +0100

    feat: refactor spawn_app for supporting PgPool

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,5 @@
 use actix_web::{post, web, HttpResponse};
+use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
 struct FormData {
@@ -7,6 +8,24 @@ struct FormData {
 }
 
 #[post("/subscriptions")]
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
