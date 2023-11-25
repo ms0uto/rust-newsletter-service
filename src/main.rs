2bb77ab2d5263db3127925301d25c3592517a025
Martín Souto <39985547+ms0uto@users.noreply.github.com>
2023-11-25 22:09:49 +0100

    Feat isolated tests (#2)
    * feat: refactor spawn_app for supporting PgPool
    
    * feat: randomize db names for test isolation
    
    * ci: fmt and clippy compliance

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use newsletter_service::configuration::get_configuration;
 use newsletter_service::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
-    run(TcpListener::bind(address).expect("Failed to bind port"))?.await
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,11 +1,18 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
-        .listen(listener)?
-        .run();
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
+        App::new()
+            .service(health_check)
+            .service(subscribe)
+            .app_data(connection.clone())
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
